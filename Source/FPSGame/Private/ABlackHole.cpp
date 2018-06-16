7d609f2d1637161f02123e1576ea7155a3c09bda
fuhao <1119369173@qq.com>
2018-06-16 15:50:52 +0800

    BlackHole

---
+++ Source/FPSGame/Private/ABlackHole.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ABlackHole.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+AABlackHole::AABlackHole()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");	
+	SetRootComponent(MeshComp);
+	SphereComp = CreateDefaultSubobject<USphereComponent>("SphereComp");
+	SphereComp->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void AABlackHole::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AABlackHole::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	TArray<UPrimitiveComponent*> Components;
+	SphereComp->GetOverlappingComponents(Components);
+
+	for (UPrimitiveComponent *Component : Components)
+	{
+		if (!Component->IsSimulatingPhysics())
+			continue;
+		Component->AddRadialForce(GetActorLocation(), SphereComp->GetScaledSphereRadius(), -2000, ERadialImpulseFalloff::RIF_Constant, true);
+	}
+}
+
